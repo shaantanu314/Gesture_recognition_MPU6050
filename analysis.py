07363c6fc0e45ffabd208a55579622c2cacaa455
shaantanu314 <shaantanu2018@gmail.com>
2021-02-09 00:05:59 +0530

    added gitignore

--- analysis.py
+++ analysis.py
@@ -30,26 +30,3 @@ for label in labels:
     plt.show()
 
 
-
-
-# print(data_set.shape)
-# for data_point in data_set:
-#     ind = np.linspace(1,300,300)
-#     x = data_point[:100]
-#     y = data_point[100:200]
-#     z = data_point[200:300]
-#     label = data_point[-1]
-#     x -= np.mean(x)
-#     y -= np.mean(y)
-#     z -= np.mean(z)
-#     traj_x , traj_y , traj_z = mt.track_3d_motion(x,y,z,interval=0.01)
-#     fig = plt.figure()
-#     ax = plt.axes(projection='3d')
-#     ax.plot3D(traj_x, traj_y, traj_z, 'gray')
-
-#     ax.scatter3D(traj_x, traj_y, traj_z, c=traj_z, cmap='Greens')
-#     ax.set_xlabel('x-axis')
-#     ax.set_ylabel('y-axis')
-#     ax.set_zlabel('z-axis')
-#     plt.show()
-    
